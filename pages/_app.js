9bd8095a8bbbfeec8681397175450abada0fc1bf
Akash Dutta <dutta.akash1993official@gmail.com>
2023-06-02 19:06:39 +0530

    some updates

--- pages/_app.js
+++ pages/_app.js
@@ -7,19 +7,19 @@ export default function Nextra({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link
+        {/* <link
           rel="alternate"
           type="application/rss+xml"
           title="RSS"
           href="/feed.xml"
-        />
-        <link
+        /> */}
+        {/* <link
           rel="preload"
           href="/fonts/Inter-roman.latin.var.woff2"
           as="font"
           type="font/woff2"
           crossOrigin="anonymous"
-        />
+        /> */}
       </Head>
       <Component {...pageProps} />
     </>
